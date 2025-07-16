6cc6e39aa7ed65b6a552441b8bb60eace9dfaba7
Dylan Carver <dylancarver14@gmail.com>
2025-07-15 23:21:22 -0500

    Set server endpoints on client app to env vars

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+  process.env.NODE_ENV === "production"
+    ? process.env.REACT_APP_PROD_SERVER_URL
+    : process.env.REACT_APP_DEV_SERVER_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
