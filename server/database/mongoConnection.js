71a72c270711f499db95b7cdda6a7fa86727676b
Dylan Carver <dylancarver14@gmail.com>
2025-07-15 23:08:57 -0500

    Log data for vercel deployment fix

--- server/database/mongoConnection.js
+++ server/database/mongoConnection.js
@@ -1,5 +1,7 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
+console.log("MONGO_DB_URI", process.env.MONGO_DB_URI);
+
 const client = new MongoClient(process.env.MONGO_DB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
